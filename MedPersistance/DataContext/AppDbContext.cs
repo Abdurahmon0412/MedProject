4c2eb760610dd8dba28019aab38466a99dd090d2
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2024-02-24 10:58:12 +0500

    add references to projects

---
+++ MedPersistance/DataContext/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MedPersistance.DataContext;
+
+public partial class MContext : DbContext
+{
+    public MContext()
+    {
+    }
+
+    public MContext(DbContextOptions<MContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Gender> Genders { get; set; }
+    public virtual DbSet<Language> Languages { get; set; }
+    public virtual DbSet<Oblast> Oblasts { get; set; }
+    public virtual DbSet<Organization> Organizations { get; set; }
+    public virtual DbSet<Region> Regions { get; set; }
+    public virtual DbSet<Role> Roles { get; set; }
+    public virtual DbSet<Roletype> RoleTypes { get; set; }
+    public virtual DbSet<Status> Statuses { get; set; }
+    public virtual DbSet<UserModule> UserModules { get; set; }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
